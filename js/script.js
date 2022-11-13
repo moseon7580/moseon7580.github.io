cd4c0b2a81cf72a3a1738ff494f2c90f1d5f170a
moseon7580 <rlaaltjs7580@naver.com>
2022-11-13 21:15:37 +0900

    모바일 헤더 수정

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function () {
+    $('.btn_menu').click(function () {
+        $(this).toggleClass('on');
+        console.log("Clicked menu");
+        $(".nav_m").toggleClass("show_list");
+        $(".nav_m").fadeIn();
     
+    });    
 }); // document.onready
