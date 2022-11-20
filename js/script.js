ec48b8d211c2501a6af137698f01004016513a4c
moseon7580 <rlaaltjs7580@naver.com>
2022-11-20 19:13:20 +0900

    works 게시물 분류 작업

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 $(function () {
-    $('.btn_menu').click(function () {
-        $(this).toggleClass('on');
-        console.log("Clicked menu");
+    // 헤더
+    $(".btn_menu").click(function () {
+        $(this).toggleClass("on");
         $(".nav_m").toggleClass("show_list");
-        $(".nav_m").fadeIn();
-    
-    });    
+    });
+
+
+    // works 게시물 분류
+    $(".work .cate .cate_menu a").click(function (e) {
+        e.preventDefault();
+
+        // 변수 중복 선언을 막기위해 let 사용 (재할당은 가능.)
+        let cate = $(this).attr("title");
+        $(".work_wr .work_item").hide();
+
+        if (cate == "all") {
+            $(".work_wr .work_item").show();
+        } else {
+            // 속성 선택자 사용
+            // [속성이름*="속성값"] 선택자는 특정 속성의 속성값에 특정 문자열를 포함하는 요소를 모두 선택.
+            $(".work_wr .work_item[class*="+cate+"]").show();
+        }
+    });
 }); // document.onready
